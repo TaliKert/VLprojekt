2c13ff1ad7d828180080f082c013be2357346072
Kert <Kert97@gmail.com>
2018-09-08 19:18:52 +0300

    initial

---
+++ src/main/java/com/kmk/imageboard/ImageboardApplication.java
@@ -0,0 +1,11 @@
+package com.kmk.imageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageboardApplication.class, args);
+    }
+}
