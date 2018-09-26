a4d6167366dc87998c6da017b95d13a624786103
Kert <Kert97@gmail.com>
2018-09-26 22:09:48 +0300

    google login

--- src/main/java/com/kmk/imageboard/ImageboardApplication.java
+++ src/main/java/com/kmk/imageboard/ImageboardApplication.java
@@ -2,14 +2,12 @@ package com.kmk.imageboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @RestController
 public class ImageboardApplication {
 
