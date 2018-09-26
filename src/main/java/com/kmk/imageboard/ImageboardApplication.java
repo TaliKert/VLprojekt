e6c5819c3567d7b4794d5d19a79b75e6d5d63a70
Kert <Kert97@gmail.com>
2018-09-26 20:02:28 +0300

    testing with google authentication

--- src/main/java/com/kmk/imageboard/ImageboardApplication.java
+++ src/main/java/com/kmk/imageboard/ImageboardApplication.java
@@ -2,9 +2,22 @@ package com.kmk.imageboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class ImageboardApplication {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ImageboardApplication.class, args);
     }
