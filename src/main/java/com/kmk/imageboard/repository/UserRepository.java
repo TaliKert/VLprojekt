2f6bb0209f0f905d5b261914009b3a55bb3fc05e
Kert <Kert97@gmail.com>
2018-09-30 16:31:25 +0300

    database

--- src/main/java/com/kmk/imageboard/repository/UserRepository.java
+++ src/main/java/com/kmk/imageboard/repository/UserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /* Läbi selle interface'i on võimalik andmebaasiga suhelda */
