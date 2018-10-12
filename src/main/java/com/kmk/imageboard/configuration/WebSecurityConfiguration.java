dc20e686fddd84d77c5aa475863295f9a529bb5c
Kert <Kert97@gmail.com>
2018-10-12 23:27:27 +0300

    made first login better

--- src/main/java/com/kmk/imageboard/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/kmk/imageboard/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableOAuth2Sso
 @Configuration
@@ -17,6 +18,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/login**", "/webjars/**", "/error**", "/css/**", "/js/**", "/img/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/");
     }
 }
