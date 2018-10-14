3bd627fa192b5506e838da9a08af803c3c2499b6
Kert <Kert97@gmail.com>
2018-10-14 21:57:10 +0300

    Image uploading works!

--- src/main/java/com/kmk/imageboard/controller/IndexController.java
+++ src/main/java/com/kmk/imageboard/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.kmk.imageboard.controller;
 
 import com.kmk.imageboard.service.UserService;
+import org.bouncycastle.math.raw.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -55,6 +56,17 @@ public class IndexController {
             model.addAttribute("username", userService.getUser(principal).getUsername());
         }
         return "statistics";
+    }
+
+    @GetMapping("/upload")
+    public String upload(Model model, Principal principal) {
+        if (principal != null) {
+            if (userService.getUser(principal) == null) {
+                return "redirect:/register";
+            }
+            model.addAttribute("username", userService.getUser(principal).getUsername());
+        }
+        return "upload";
     }
 
     @RequestMapping("/user")
